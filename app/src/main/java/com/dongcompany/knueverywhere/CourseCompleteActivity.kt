9eba1fbd5cd8ce1e7c99d28b701bad9f8634c68f
GE60 <dhyun0601@naver.com>
2020-11-09 21:37:23 +0900

    201109 탐방 완료 시 이펙트, 앱 아이콘

--- app/src/main/java/com/dongcompany/knueverywhere/CourseCompleteActivity.kt
+++ app/src/main/java/com/dongcompany/knueverywhere/CourseCompleteActivity.kt
@@ -1,11 +1,30 @@
 package com.dongcompany.knueverywhere
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+
 
 class CourseCompleteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_complete)
+            val m: MediaPlayer = MediaPlayer.create(this, R.raw.sound)
+            m.start()
+
+
+        val imageView:ImageView = findViewById(R.id.CourseCompleteActivity_Imageview)
+        Glide.with(this)
+                .load(R.raw.dance)
+                .into(imageView)
+
+        findViewById<Button>(R.id.CourseCompleteActivity_Button).setOnClickListener(View.OnClickListener {
+            finish()
+        })
     }
 }
